4ba55f8f5bf280acfd2ecc20c92ededa91190670
Fabian Ferno <fabianferno@gmail.com>
2021-07-21 14:31:07 +0530

    proto db

--- config.py
+++ config.py
@@ -7,9 +7,11 @@ from pymysql.cursors import DictCursor
 # Rows from cursors will always be of type dict || cursorclass=DictCursor
 mysql = MySQL(cursorclass=DictCursor)
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = ""
-app.config['MYSQL_DATABASE_PASSWORD'] = ""
-app.config['MYSQL_DATABASE_DB'] = ""
-app.config['MYSQL_DATABASE_HOST'] = ""
+# MySQL configurations and Init
+app.config['MYSQL_DATABASE_USER'] = "tux"
+app.config['MYSQL_DATABASE_PASSWORD'] = "licet@123"
+app.config['MYSQL_DATABASE_DB'] = "fabian"
+app.config['MYSQL_DATABASE_HOST'] = "opencloud.pattarai.in"
+app.config['MYSQL_DATABASE_PORT'] = "8306"
 mysql.init_app(app)
+
