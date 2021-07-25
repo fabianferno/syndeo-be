ef9cf596426c854351a81be49aa63274293df9ce
Praveen <praveenrijal.23it@licet.ac.in>
2021-07-25 18:51:27 +0530

    Errors fixed

--- routes/allocations.py
+++ routes/allocations.py
@@ -3,63 +3,67 @@ from app import app, forbidden, internal_server_error
 from config import mysql
 from flask import jsonify, request
 
-#/allocations
-@app.route('/allocations',methods = ['POST'],['GET'])
+# /allocations
+
+
+@app.route('/allocations', methods=['POST', 'GET'])
 def allocations():
-"""    
-    [POST] - Insert allocation records 
-"""
+
+    """
+    [POST] - Insert allocation records
+    """
     try:
-        if _uid and request.method == 'POST':
+            if  request.method == 'POST':
 
+                _uid = request.form['uid']
+                _allocationId = request.form['allocationId']
+                _mentorUid = request.form['mentorUid']
+                _menteeUid = request.form['menteeUid']
+                _dateAllocated = request.form['dateAllocated']
+                _isValidated = request.form['isValidated']
+                _isAgreed = request.form['isAgreed']
+                _validator = request.form['validator']
 
-            _uid = request.form['uid']
-            _allocationId = request.form['allocationId']
-            _mentorUid = request.form['mentorUid']
-            _menteeUid = request.form['menteeUid']
-            _dateAllocated = request.form['dateAllocated']
-            _isValidated = request.form['isValidated']
-            _isAgreed = request.form['isAgreed']
-            _validator = request.form['validator']
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-
-            cursor.execute(
-                f"INSERT INTO allocations(uid, allocationId, mentorUid, menteeUid, dateAllocated, isValidated, isAgreed, validator) VALUES('{_uid}','{_allocationId}', '{_mentorUid}', '{_menteeUid}', '{_dateAllocated}', '{_isValidated}', '{_isAgreed}', '{_validator}',)")
-            conn.commit()
-            res = jsonify('success')
-            res.status_code = 200
-            return res
-
-"""
-    [GET][admin] - Get a List of allocations
-"""
-
-        if request.method == 'GET':
-            _allocationId = request.args['allocationId']
-            
                 conn = mysql.connect()
                 cursor = conn.cursor()
-                cursor.execute(f"SELECT * FROM `admin` WHERE `admin`.`uid` = '{_uid}'")
-                admin = cursor.fetchone()
 
-            if admin: 
-                cursor.execute(f"SELECT * FROM `allocations` WHERE `allocations`.`allocationId` = '{_allocationId}'")
-
-            else:
-                return forbidden()  # It throws a 403 response saying "failure"
-                allocationRecord = cursor.fetchone()
-
-                cursor.close()
-                conn.close()
-
-                res = jsonify(allocationRecord)
+                cursor.execute(
+                    f"INSERT INTO allocations(uid, allocationId, mentorUid, menteeUid, dateAllocated, isValidated, isAgreed, validator) VALUES('{_uid}','{_allocationId}', '{_mentorUid}', '{_menteeUid}', '{_dateAllocated}', '{_isValidated}', '{_isAgreed}', '{_validator}',)")
+                conn.commit()
+                res = jsonify('success')
                 res.status_code = 200
                 return res
 
-        else:
-            return forbidden()  # It throws a 403 response saying "failure"
+    
+    #     [GET][admin] - Get a List of allocations
+    
+
+            if request.method == 'GET':
+                _allocationId = request.args['allocationId']
+
+                conn = mysql.connect()
+                cursor = conn.cursor()
+                cursor.execute(
+                                f"SELECT * FROM `admin` WHERE `admin`.`uid` = '{_uid}'")
+                admin = cursor.fetchone()
+
+                if admin:
+                    cursor.execute(
+                                f"SELECT * FROM `allocations` WHERE `allocations`.`allocationId` = '{_allocationId}'")
+
+                else:
+                    return forbidden()  # It throws a 403 response saying "failure"
+                    allocationRecord = cursor.fetchone()
+
+                    cursor.close()
+                    conn.close()
+
+                    res = jsonify(allocationRecord)
+                    res.status_code = 200
+                    return res
+
+            else:
+                return forbidden()  # It throws a 403 response saying "failure"
 
     except Exception as e:
         print(e)
@@ -67,42 +71,42 @@ def allocations():
 
 
 
-#/allocations?<uid>
+    # /allocations?<uid>
 
-@app.route('/allocations?<uid>', methods = ['GET'])
-def allocation(uid):
+    @app.route('/allocations?<uid>', methods = ['GET'])
+    def allocation(uid):
 
-"""
-    [GET][admin] - Get allocation record where mentorUid / menteerUid == uid
-"""
+        """
+        [GET][admin] - Get allocation record where mentorUid / menteerUid == uid
+        """
 
-try:
-    if request.method == 'GET':
-        _allocationId = request.args['allocationId']
-            
+        try:
+            if request.method == 'GET':
+                _allocationId = request.args['allocationId']
+                    
                 conn = mysql.connect()
                 cursor = conn.cursor()
                 cursor.execute(f"SELECT * FROM `admin` WHERE `admin`.`uid` = '{_uid}'")
                 admin = cursor.fetchone()
 
-            if admin: 
-                cursor.execute(f"SELECT * FROM `allocations` WHERE `allocations` . `mentorId` = '{_uid}' AND `menteeId` = '{_uid}' ")
-            
+                if admin: 
+                    cursor.execute(f"SELECT * FROM `allocations` WHERE `allocations` . `mentorId` = '{_uid}' AND `menteeId` = '{_uid}' ")
+                    
+
+                else:
+                    return forbidden()  # It throws a 403 response saying "failure"
+                    allocationRecord = cursor.fetchone()
+
+                    cursor.close()
+                    conn.close()
+
+                    res = jsonify(allocationRecord)
+                    res.status_code = 200
+                    return res
 
             else:
                 return forbidden()  # It throws a 403 response saying "failure"
-                allocationRecord = cursor.fetchone()
 
-                cursor.close()
-                conn.close()
-
-                res = jsonify(allocationRecord)
-                res.status_code = 200
-                return res
-
-    else:
-        return forbidden()  # It throws a 403 response saying "failure"
-
-except Exception as e:
-    print(e)
-    return internal_server_error()
+        except Exception as e:
+            print(e)
+            return internal_server_error()
